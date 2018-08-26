eadf05af54e8aa4619862aad9e4f13a3b980a023
Simon Stettler <simon.stettler@gmail.com>
2018-08-26 15:11:58 +0200

    xml schema validation

--- GenerateMetaRunner/Resources.cs
+++ GenerateMetaRunner/Resources.cs
@@ -5,5 +5,7 @@ namespace GenerateMetaRunner
     public class Resources
     {
         public static Stream TemplateXml => typeof(Resources).Assembly.GetManifestResourceStream($"{typeof(Resources).Namespace}.template.xml");
+
+        public static Stream MetaRunnerXsd => typeof(Resources).Assembly.GetManifestResourceStream($"{typeof(Resources).Namespace}.meta-runner.xsd");
     }
 }
---
+++ NUnit/XmlTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Runtime.Remoting.Channels;
+using System.Xml;
+using System.Xml.Schema;
+using GenerateMetaRunner;
+using NUnit.Framework;
+
+namespace NUnit
+{
+    [TestFixture]
+    public class XmlTests
+    {
+        [Test]
+        public static void TestSchemaValidation()
+        {
+            ValidationEventHandler validationEventHandler= (sender, args) =>
+            {
+                if (args.Severity == XmlSeverityType.Warning)
+                {
+                    Assert.Inconclusive(args.Message);
+                }
+                else
+                {
+                    Assert.Fail(args.Message);
+                }
+            };
+
+            var settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationFlags = 
+                settings.ValidationFlags | 
+                XmlSchemaValidationFlags.ProcessInlineSchema | 
+                XmlSchemaValidationFlags.ProcessSchemaLocation | 
+                XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationEventHandler += validationEventHandler;
+            
+            var schema = XmlSchema.Read(Resources.MetaRunnerXsd, validationEventHandler);
+            settings.Schemas.Add(schema);
+                
+            var reader = XmlReader.Create(Resources.TemplateXml, settings);
+            while (reader.Read())
+            {
+                Assert.IsTrue(true);
+            };
+        }
+    }
+}
