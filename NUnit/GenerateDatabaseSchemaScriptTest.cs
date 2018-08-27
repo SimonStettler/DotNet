c2ef6021474f56a71585c5be3a4193f3c933b209
SimonStettler <simon.stettler@prolistic.ch>
2018-08-27 18:13:44 +0200

    add missing file

---
+++ NUnit/GenerateDatabaseSchemaScriptTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace NUnit
+{
+    [TestFixture]
+    public class GenerateDatabaseSchemaScriptTest
+    {
+        [Test]
+        public void TestMain()
+        {
+            var name = "master";
+            Assert.DoesNotThrow(() => Program.Main(new [] {name}));
+        }
+    }
+}
