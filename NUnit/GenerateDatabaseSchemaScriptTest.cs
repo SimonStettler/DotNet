4ae4518f5bb8d1c94dc3e69e563bb7674fd25050
SimonStettler <simon.stettler@prolistic.ch>
2018-08-27 18:34:11 +0200

    negative test

--- NUnit/GenerateDatabaseSchemaScriptTest.cs
+++ NUnit/GenerateDatabaseSchemaScriptTest.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+using System.IO;
 
 namespace NUnit
 {
@@ -10,6 +12,14 @@ namespace NUnit
         {
             var name = "master";
             Assert.DoesNotThrow(() => Program.Main(new [] {name}));
+            Assert.That(File.Exists($"{name}.sql"));
+        }
+
+        [Test]
+        public void TestFailure()
+        {
+            var name = "slave";
+            Assert.Throws<Exception>(() => Program.Main(new [] {name}));
         }
     }
 }
